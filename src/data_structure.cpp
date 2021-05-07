d94cb340b2a6839f6a4bf0967fa78796f1068efb
Marat <gG3-Kg4-V0E-aAX>
2021-05-08 00:05:58 +0300

    First attempt of method realisation

--- src/data_structure.cpp
+++ src/data_structure.cpp
@@ -1,9 +1,59 @@
 #include "data_structure.hpp"
+#include <iostream>
+#include <stack>
 
 // файл с определениями
 
 namespace itis {
 
   // здесь должны быть определения методов вашей структуры
+
+  void Graph::addEdge(int v, int w) {
+    adj_m[v].push_back(w);
+  }
+
+  Graph::Graph(int numberOfVertex) {
+    this->_numberOfVertex = numberOfVertex;
+    adj_m = new list<int>[numberOfVertex];
+  }
+
+  Graph::~Graph() {}
+
+
+
+  void Graph::topologicalSort() {
+
+   // vector<int>answer;
+    stack<int> Stack;
+
+    bool *visited = new bool [_numberOfVertex];
+
+    for( int i = 0; i < _numberOfVertex; i++){
+      visited[i] = false;
+    }
+
+    for (int i = 0; i < _numberOfVertex; i++){
+      if (!visited[i])
+        depthFirstSearch(i, visited, Stack);
+    }
+    cout<<Stack.empty()<<endl;
+    while (!Stack.empty()){
+
+      cout << Stack.top() << " "<< endl;
+     Stack.pop();
+    }
+
+  }
+  void Graph::depthFirstSearch(int v, bool *visited, stack<int> &Stack) {
+    for (_List_iterator<int> i =adj_m->begin() ; i !=adj_m->end(); ++i) {
+      if (!visited[*i]){
+        depthFirstSearch(*i,visited,Stack);
+      }
+    }
+
+    Stack.push(v);
+    cout<<Stack.size()<<endl;
+  }
+
 
 }  // namespace itis
